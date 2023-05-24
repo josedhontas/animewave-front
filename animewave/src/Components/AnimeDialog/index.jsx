c45e05555accc2b990bc3788230274752fc1006e
josedhonatas <dhonatas@academico.ufs.br>
2023-05-24 19:11:09 -0300

    oringes

--- animewave/src/Components/AnimeDialog/index.jsx
+++ animewave/src/Components/AnimeDialog/index.jsx
@@ -49,9 +49,7 @@ BootstrapDialogTitle.propTypes = {
 
 export default function AnimeDialog({ linkepisodio, onClose }) {
   const [open, setOpen] = React.useState(true);
-  linkepisodio = "https://lightspeedst.net/s3/mp4/death-note/sd/1.mp4";
 
-  
   const handleClose = () => {
     setOpen(false);
     onClose(); // Chama a função onClose fornecida como prop

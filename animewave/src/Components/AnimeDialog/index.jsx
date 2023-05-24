4c635460b25c4b4dc9d552ffb41d047f6d350287
josedhonatas <dhonatas@academico.ufs.br>
2023-05-24 19:27:44 -0300

    teste2

--- animewave/src/Components/AnimeDialog/index.jsx
+++ animewave/src/Components/AnimeDialog/index.jsx
@@ -67,7 +67,7 @@ export default function AnimeDialog({ linkepisodio, onClose }) {
         </BootstrapDialogTitle>
         <DialogContent dividers>
           <video controls>
-            <source src={linkepisodio} type="video/mp4" />
+            <source src="https://c-an-ca1.betterstream.cc:2223/hls-playback/a36ed4227720a477b9b763ba35e0225948ff459da3fea48138bc95350498f3d4f1834408ed8063d221a0fc1747f7ab3bfee4a565b6701b595f3e1bde3f96c19443bb84a2feda9ae258fc89224946eea1373234bc90f6ecb0c86c6c2226e9a157db57c2a645c2481338a343640f97a38f743766bb6529c58c195abe3cb042ba9ab6eac725ca3b60909f64703b6051cd6390d3c09440b0c077d1d696c3403677045fc154c34a992c6da0dbc347920cc8e7/index-f1-v1-a1.m3u8" type="application/x-mpegURL" />
             Seu navegador não suporta a reprodução de vídeo.
           </video>
         </DialogContent>

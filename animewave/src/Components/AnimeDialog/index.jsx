ea65b398d2be4da1dc90f5526d4e68029eba70f7
josedhonatas <dhonatas@academico.ufs.br>
2023-05-24 20:01:44 -0300

    mudanca_experimental

--- animewave/src/Components/AnimeDialog/index.jsx
+++ animewave/src/Components/AnimeDialog/index.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
@@ -47,33 +47,35 @@ BootstrapDialogTitle.propTypes = {
   onClose: PropTypes.func.isRequired,
 };
 
-export default function AnimeDialog({ linkepisodio, onClose }) {
-  const [open, setOpen] = React.useState(true);
+export default function AnimeDialog({ linkepisodio = "https://tc-1.boocdn.net/_v6/a36ed4227720a477b9b763ba35e0225948ff459da3fea48138bc95350498f3d4f1834408ed8063d221a0fc1747f7ab3bf2fc613dcc5dcbedc116815be3c76181768733ef68abea714f2d01065e813088f69259fe1d7a84c4cf362a5933a34bf0f7b01d4b5611dc8cad5f6a3fd3c1cf9971c3ee258e1c6b0c1a6a9dfab9edf106/index-f1-v1-a1.m3u8", onClose }) {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.src = linkepisodio;
+      video.load();
+    }
+  }, [linkepisodio]);
 
   const handleClose = () => {
-    setOpen(false);
-    onClose(); // Chama a função onClose fornecida como prop
-  };
-
-  const handleClickOpen = () => {
-    setOpen(true);
+    onClose();
   };
 
   return (
     <div>
-      <BootstrapDialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+      <BootstrapDialog onClose={handleClose} aria-labelledby="customized-dialog-title" open>
         <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
           AnimeWave
         </BootstrapDialogTitle>
         <DialogContent dividers>
-          <video controls>
-            <source src="https://c-an-ca1.betterstream.cc:2223/hls-playback/a36ed4227720a477b9b763ba35e0225948ff459da3fea48138bc95350498f3d4f1834408ed8063d221a0fc1747f7ab3bfee4a565b6701b595f3e1bde3f96c19443bb84a2feda9ae258fc89224946eea1373234bc90f6ecb0c86c6c2226e9a157db57c2a645c2481338a343640f97a38f743766bb6529c58c195abe3cb042ba9ab6eac725ca3b60909f64703b6051cd6390d3c09440b0c077d1d696c3403677045fc154c34a992c6da0dbc347920cc8e7/index-f1-v1-a1.m3u8" type="application/x-mpegURL" />
-            Seu navegador não suporta a reprodução de vídeo.
-          </video>
+          <div>
+            <video ref={videoRef} className="video-js vjs-default-skin" controls playsInline />
+          </div>
         </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={handleClose}>
-            Próximo(ainda nao funciona)
+            Próximo (ainda não funciona)
           </Button>
         </DialogActions>
       </BootstrapDialog>

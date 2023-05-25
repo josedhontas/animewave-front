fcfe6ca4de10fea59bda629b22fd7f8c83632cc2
josedhonatas <dhonatas@academico.ufs.br>
2023-05-25 14:56:28 -0300

    remo

--- animewave/src/Components/AnimeDialog/index.jsx
+++ animewave/src/Components/AnimeDialog/index.jsx
@@ -8,7 +8,6 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-import chihiro from './chihiro.mp4'
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
